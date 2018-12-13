e1bfbd5bff1596869a588b436671581c099cbe43
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2018-12-13 14:29:30 -0800

    moved to npm package

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require("@babel/polyfill");
 // const Web3 = require('web3')
 const TransportHID = require('@ledgerhq/hw-transport-node-hid').default
 // const TransportU2F = require("@ledgerhq/hw-transport-u2f").default
-const ProviderEngine = require('web3-provider-engine')
+const ProviderEngine = require('@deconet/web3-provider-engine')
 const ProviderSubprovider = require('web3-provider-engine/subproviders/provider.js')
 const RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js')
 const InfuraSubprovider = require('web3-provider-engine/subproviders/infura.js')
 const FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js')
 const NonceSubprovider = require('web3-provider-engine/subproviders/nonce-tracker.js')
-const createLedgerSubprovider = require('@ledgerhq/web3-subprovider').default
+const createLedgerSubprovider = require('@deconet/web3-subprovider').default
 
 
 class LedgerProvider {
